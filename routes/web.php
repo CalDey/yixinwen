c28122efe525937fe4a6f43a10ff828854cc0f73
vagrant <vagrant@homestead>
2020-11-09 07:35:24 +0000

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/','PagesController@root')->name('root');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
