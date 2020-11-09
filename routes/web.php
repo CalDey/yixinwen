422933750dd7837b5396a57c031b0cdbc3c2b782
vagrant <vagrant@homestead>
2020-11-09 13:06:12 +0000

    生成文章骨架

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('articles', 'ArticlesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
