d342ab56565fd59dcc5dcac4ac0ec963018d3ead
vagrant <vagrant@homestead>
2020-11-10 10:22:50 +0000

    文章详情页

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,63 +1,61 @@
 @extends('layouts.app')
 
+@section('title', $article->title)
+@section('description', $article->excerpt)
+
 @section('content')
 
-<div class="container">
-  <div class="col-md-10 offset-md-1">
-    <div class="card ">
-      <div class="card-header">
-        <h1>Article / Show #{{ $article->id }}</h1>
-      </div>
+  <div class="row">
 
-      <div class="card-body">
-        <div class="card-block bg-light">
-          <div class="row">
-            <div class="col-md-6">
-              <a class="btn btn-link" href="{{ route('articles.index') }}"><- Back</a>
-            </div>
-            <div class="col-md-6">
-              <a class="btn btn-sm btn-warning float-right mt-1" href="{{ route('articles.edit', $article->id) }}">
-                Edit
+    <div class="col-lg-3 col-md-3 hidden-sm hidden-xs author-info">
+      <div class="card ">
+        <div class="card-body">
+          <div class="text-center">
+            作者：{{ $article->user->name }}
+          </div>
+          <hr>
+          <div class="media">
+            <div align="center">
+              <a href="{{ route('users.show', $article->user->id) }}">
+                <img class="thumbnail img-fluid" src="{{ $article->user->avatar }}" width="300px" height="300px">
               </a>
             </div>
           </div>
         </div>
-        <br>
+      </div>
+    </div>
 
-        <label>Title</label>
-<p>
-	{{ $article->title }}
-</p> <label>Body</label>
-<p>
-	{{ $article->body }}
-</p> <label>User_id</label>
-<p>
-	{{ $article->user_id }}
-</p> <label>Category_id</label>
-<p>
-	{{ $article->category_id }}
-</p> <label>Reply_count</label>
-<p>
-	{{ $article->reply_count }}
-</p> <label>View_count</label>
-<p>
-	{{ $article->view_count }}
-</p> <label>Last_reply_user_id</label>
-<p>
-	{{ $article->last_reply_user_id }}
-</p> <label>Order</label>
-<p>
-	{{ $article->order }}
-</p> <label>Excerpt</label>
-<p>
-	{{ $article->excerpt }}
-</p> <label>Slug</label>
-<p>
-	{{ $article->slug }}
-</p>
+    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 article-content">
+      <div class="card ">
+        <div class="card-body">
+          <h1 class="text-center mt-3 mb-3">
+            {{ $article->title }}
+          </h1>
+
+          <div class="article-meta text-center text-secondary">
+            <i class="far fa-clock"></i>
+            {{ $article->created_at->diffForHumans() }}
+            {{-- ⋅
+            <i class="far fa-comment"></i>
+            {{ $article->reply_count }} --}}
+          </div>
+
+          <div class="article-body mt-4 mb-4">
+            {!! $article->body !!}
+          </div>
+
+          <div class="operate">
+            <hr>
+            <a href="{{ route('articles.edit', $article->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
+              <i class="far fa-edit"></i> 编辑
+            </a>
+            <a href="#" class="btn btn-outline-secondary btn-sm" role="button">
+              <i class="far fa-trash-alt"></i> 删除
+            </a>
+          </div>
+
+        </div>
       </div>
     </div>
   </div>
-</div>
-
-@endsection
+@stop
