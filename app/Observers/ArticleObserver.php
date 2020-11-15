f3985086701f9649b8a71fff21280dd2ee2866a4
vagrant <vagrant@homestead>
2020-11-15 06:37:23 +0000

    翻译队列化

--- app/Observers/ArticleObserver.php
+++ app/Observers/ArticleObserver.php
@@ -3,7 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Article;
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -18,10 +18,20 @@ class ArticleObserver
         // 生成话题摘录
         $article->excerpt = make_excerpt($article->body);
 
-        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
-        if ( ! $article->slug) {
-            $article->slug = app(SlugTranslateHandler::class)->translate($article->title);
-        }
     }
+
+    public function saved(Article $article)
+    {
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( !$article->slug) {
+
+            // $article->slug = app(SlugTranslateHandler::class)->translate($article->title);
+
+            // 推送任务到队列
+            dispatch(new TranslateSlug($article));
+        }
+
+    }
+
 
 }
